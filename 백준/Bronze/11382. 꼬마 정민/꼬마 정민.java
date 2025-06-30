00a9114596dba55988d614a0ecbc167cdc2aac0e
xo-rxs <taekyun0525@gmail.com>
2025-06-30 17:15:52 +0900

    [Bronze V] Title: 꼬마 정민, Time: 156 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    long num = sc.nextLong();
+    long num1 = sc.nextLong();
+    long num2 = sc.nextLong();
+    System.out.println(num + num1 + num2);
+    }
+}
