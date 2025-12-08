3366fe42c17ddfe4c171d804495fec7e4f080e71
xo-rxs <taekyun0525@gmail.com>
2025-12-08 19:29:09 +0900

    [Silver IV] Title: ATM, Time: 216 ms, Memory: 19580 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int prev = 0;
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += prev + arr[i];
+
+            prev += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
